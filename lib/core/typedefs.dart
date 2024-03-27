991019f3b9ea28185b6614b5469c49ec4e94e3d5
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-27 15:49:45 -0500

    ✨ (typedefs.dart): introduce new FutureAuthResult generic type to handle asynchronous authentication results in a more descriptive way

--- lib/core/typedefs.dart
+++ lib/core/typedefs.dart
@@ -4,5 +4,6 @@ import '../failures/failure.dart';
 import 'result.dart';
 
 typedef Json = Map<String, dynamic>;
+typedef FutureAuthResult<T, E> = Future<Result<T, E>>;
 typedef FutureResult<T> = Future<Result<T, Failure>>;
 typedef FriendshipsData = ({AppUser user, Friendship? friendship});
