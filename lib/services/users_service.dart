2006d0ce9fc7056fce22b71c3ba0b50ee49ca186
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-27 15:34:41 -0500

    📝 (users_service.dart): update getUserById method to return a Result type to handle success and error cases more effectively

--- lib/services/users_service.dart
+++ lib/services/users_service.dart
@@ -1,21 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../core/result.dart';
 import '../core/typedefs.dart';
 import '../entities/app_user.dart';
 import '../extensions/document_snapshot_x.dart';
+import '../failures/failure.dart';
 
 extension type UsersService(FirebaseFirestore _db) {
   CollectionReference<Json> get _collection => _db.collection('users');
 
-  Future<AppUser?> getUserById(String id) async {
+  FutureResult<AppUser> getUserById(String id) async {
     try {
       final snapshot = await _collection.doc(id).get();
       if (!snapshot.exists) {
-        return null;
+        return Error(Failure(message: 'User no exists'));
       }
-      return snapshot.toAppUser();
-    } catch (_) {
-      return null;
+      return Success(snapshot.toAppUser());
+    } catch (e) {
+      return Error(Failure(message: e.toString()));
     }
   }
 
