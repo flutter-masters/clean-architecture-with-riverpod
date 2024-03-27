4d3424caf7032866d9c90e99e31dd57b53f55b20
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-27 15:33:11 -0500

    ✨ (typedefs.dart): introduce new typedefs for FutureResult, FriendshipsData, and update existing typedefs for Json to improve code readability and maintainability

--- lib/core/typedefs.dart
+++ lib/core/typedefs.dart
@@ -1,1 +1,8 @@
+import '../entities/app_user.dart';
+import '../entities/friendship.dart';
+import '../failures/failure.dart';
+import 'result.dart';
+
 typedef Json = Map<String, dynamic>;
+typedef FutureResult<T> = Future<Result<T, Failure>>;
+typedef FriendshipsData = ({AppUser user, Friendship? friendship});
