ea4e400b1d85e404c2df06e4a63df01fdcac7ac7
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-03-27 15:34:33 -0500

    ✨ (friends_tab.dart): Introduce sealed classes for different states in the FriendsTab widget to manage the UI state more effectively 🔧 (friends_tab.dart): Update imports and class names to reflect changes in the project structure and improve code organization and readability 🔧 (friends_tab.dart): Refactor the FriendsTab widget to use a state object instead of individual variables for better state management and readability 🔧 (friends_tab.dart): Refactor the loadFriends and onDelete methods to update the state based on the result of the operations for better UI feedback and error handling

--- lib/ui/screens/home/tabs/friends/friends_tab.dart
+++ lib/ui/screens/home/tabs/friends/friends_tab.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../entities/app_user.dart';
+import '../../../../../core/result.dart';
+import '../../../../../core/typedefs.dart';
+import '../../../../../failures/failure.dart';
 import '../../../../../services/auth_service.dart';
 import '../../../../../services/friendships_service.dart';
 import '../../../../shared/dialogs/loader_dialog.dart';
@@ -12,6 +14,24 @@ import '../../../../shared/widgets/user_tile.dart';
 import '../../../search/search_screen.dart';
 import 'widgets/app_bar.dart';
 
+sealed class FriendsState {
+  const FriendsState();
+}
+
+class FriendsLoadingState extends FriendsState {
+  const FriendsLoadingState();
+}
+
+class FriendsLoadedState extends FriendsState {
+  const FriendsLoadedState({required this.friendsData});
+  final List<FriendshipsData> friendsData;
+}
+
+class FriendsLoadErrorState extends FriendsState {
+  const FriendsLoadErrorState({required this.failure});
+  final Failure failure;
+}
+
 class FriendsTab extends StatefulWidget {
   const FriendsTab({super.key});
 
@@ -20,9 +40,7 @@ class FriendsTab extends StatefulWidget {
 }
 
 class _FriendsTabState extends State<FriendsTab> {
-  var loading = true;
-  final friends = <AppUser>[];
-  final friendships = <String, String>{};
+  FriendsState state = const FriendsLoadingState();
   final friendshipsService = FriendshipsService(FirebaseFirestore.instance);
   final userService = AuthService(FirebaseAuth.instance);
   late final userId = userService.currentUser!.id;
@@ -35,27 +53,36 @@ class _FriendsTabState extends State<FriendsTab> {
 
   Future<void> loadFriends(Duration _) async {
     final result = await friendshipsService.getFriends(userId);
-    if (result.friends != null) {
-      friends.addAll(result.friends!);
-    }
-    if (result.friendships != null) {
-      friendships.addAll(result.friendships!);
-    }
-    loading = false;
+    state = switch (result) {
+      Success(value: final friendsData) => FriendsLoadedState(
+          friendsData: friendsData,
+        ),
+      Error(value: final failure) => FriendsLoadErrorState(
+          failure: failure,
+        ),
+    };
     setState(() {});
   }
 
-  Future<void> onDelete(AppUser friend) async {
+  Future<void> onDelete(FriendshipsData friendshipData) async {
     final deleted = await showLoader(
       context,
-      friendshipsService.deleteFriendship(friendships[friend.id]!),
+      friendshipsService.cancelFriendshipRequest(
+        friendshipData.friendship!.id,
+      ),
     );
-    if (deleted) {
-      friends.remove(friend);
-      friendships.remove(friend.id);
-      setState(() {});
-    }
+    final friendsData = switch (deleted) {
+      Success() => [...data]..remove(friendshipData),
+      Error() => data,
+    };
+    state = FriendsLoadedState(friendsData: friendsData);
+    setState(() {});
   }
+
+  List<FriendshipsData> get data => switch (state) {
+        FriendsLoadedState(friendsData: final friendsData) => friendsData,
+        _ => [],
+      };
 
   @override
   Widget build(BuildContext context) {
@@ -65,25 +92,27 @@ class _FriendsTabState extends State<FriendsTab> {
         children: [
           FriendsAppbar(onAdd: () => context.pushNamed(SearchScreen.route)),
           Expanded(
-            child: Builder(
-              builder: (_) {
-                if (loading) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (friends.isEmpty) {
-                  return const Center(
-                    child: Text("You still don't have friends"),
-                  );
-                }
-                return UserList(
-                  data: friends,
-                  itemBuilder: (_, user) => UserTile(
-                    onPressed: () => onDelete(user),
-                    user: user,
+            child: switch (state) {
+              FriendsLoadingState() => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              FriendsLoadedState(friendsData: final friendsData)
+                  when friendsData.isEmpty =>
+                const Center(
+                  child: Text("You still don't have friends"),
+                ),
+              FriendsLoadedState(friendsData: final friendsData) =>
+                UserList<FriendshipsData>(
+                  data: friendsData,
+                  itemBuilder: (_, friendData) => UserTile(
+                    onPressed: () => onDelete(friendData),
+                    user: friendData.user,
                   ),
-                );
-              },
-            ),
+                ),
+              FriendsLoadErrorState(failure: final failure) => Center(
+                  child: Text(failure.message),
+                ),
+            },
           ),
         ],
       ),
